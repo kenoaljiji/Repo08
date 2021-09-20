01f6d68c7925277f6a99a3723537f236ef0d5881
kenoaljiji <studioaljiji@gmail.com>
2021-09-20 22:23:58 +0200

    Returning Boolean Values

---
+++ ex09/src/index.js
@@ -0,0 +1,14 @@
+function isLess(a, b) {
+  //Only change code below this line
+  if (a < b) {
+    return true;
+  } else {
+    return false;
+  }
+  //Only change code above this line
+}
+
+console.log(isLess(10, 15));
+console.log(isLess(15, 10));
+
+module.exports = isLess;
