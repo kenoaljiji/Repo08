d7254a1c46f8b1985c073936437ef2f219bc4286
kenoaljiji <studioaljiji@gmail.com>
2021-09-20 20:29:24 +0200

    Print function with two arguments

---
+++ ex02/src/index.js
@@ -0,0 +1,9 @@
+function functionPrintInfo(name, age) {
+  //Only change code below this line
+  console.log(name, age);
+  //Only change code above this line
+}
+functionPrintInfo("John", 23);
+functionPrintInfo("Michael", 43);
+
+module.exports = functionPrintInfo;
