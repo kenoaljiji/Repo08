7f280b97824b3920bd05bc28c04d84f05a0e2214
kenoaljiji <studioaljiji@gmail.com>
2021-09-20 23:07:14 +0200

    add random number

---
+++ ex12/src/index.js
@@ -0,0 +1,8 @@
+function randomInteger() {
+  //Only change code below this line
+
+  //Only change code above this line
+  return Math.floor(Math.random() * 30);
+}
+
+console.log(randomInteger());
