2734976620126cd848615ecedb355792bbfca178
kenoaljiji <studioaljiji@gmail.com>
2021-09-20 20:21:47 +0200

    add reusable function

---
+++ ex01/src/index.js
@@ -0,0 +1,8 @@
+function reusableFunction() {
+  //Only change code below this line
+  console.log("Hi Padawan!");
+  //Only change code above this line
+}
+reusableFunction();
+
+module.exports = reusableFunction;
