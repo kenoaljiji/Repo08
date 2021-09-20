008546c22f9aeaf3d0cfa3ea358f840d712879ed
kenoaljiji <studioaljiji@gmail.com>
2021-09-20 22:17:30 +0200

    return function and assign to variable total

---
+++ ex08/src/index.js
@@ -0,0 +1,14 @@
+var total = 0;
+
+function funcWithArg(digit) {
+  //Only change code below this line
+
+  return (digit + 9) / 5;
+
+  //Only change code above this line
+}
+
+total = funcWithArg(6);
+console.log(total);
+
+module.exports = funcWithArg;
