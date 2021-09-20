86670719f831520c3a4ff9c1185eae3f24277a69
kenoaljiji <studioaljiji@gmail.com>
2021-09-20 23:28:27 +0200

    add string number  to integer

---
+++ ex14/src/index.js
@@ -0,0 +1,12 @@
+function convertStrToInt(str) {
+  //Only change code below this line
+
+  //Only change code above this line
+  return parseInt(str);
+}
+
+console.log(convertStrToInt("44"));
+console.log(convertStrToInt("27"));
+console.log(convertStrToInt("JeffBezos"));
+
+module.exports = convertStrToInt;
