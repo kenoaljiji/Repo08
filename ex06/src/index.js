7d89cc809fd23925a9cfebd4c4af4bb0d895f5a5
kenoaljiji <studioaljiji@gmail.com>
2021-09-20 21:29:17 +0200

    multiple number with functions and arguments

---
+++ ex06/src/index.js
@@ -0,0 +1,15 @@
+function xSix(num) {
+  //Only change code below this line
+  //Only change code above this line
+  return num * 6;
+}
+
+//Only change code below this line
+
+console.log(xSix(5));
+console.log(xSix(2));
+console.log(xSix(0));
+
+//Only change code above this line
+
+module.exports = xSix;
