76b6c12c5b2edd42eecb305e99b82b80c9ca07eb
kenoaljiji <studioaljiji@gmail.com>
2021-09-20 23:34:19 +0200

    parsein function with radiax

---
+++ ex15/src/index.js
@@ -0,0 +1,12 @@
+function convertStrToInt(str) {
+  //Only change code below this line
+
+  //Only change code above this line
+  return parseInt(str, 16);
+}
+
+console.log(convertStrToInt("BA"));
+console.log(convertStrToInt("F1"));
+console.log(convertStrToInt("JeffBezos"));
+
+module.exports = convertStrToInt;
